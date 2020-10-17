6bf32c101971a2bfcc3b4217b6ac7faba7575447
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-17 15:52:21 -0500

    sourced jquery, client.js, and styles.css in index.html

--- database.sql
+++ database.sql
@@ -1,5 +1,6 @@
-CREATE TABLE "tasks" (
+CREATE TABLE "tasksToDo" (
 "id" SERIAL PRIMARY KEY,
+"person" VARCHAR (50) NOT NULL,
 "taskName" VARCHAR(50) NOT NULL,
 "taskNotes" VARCHAR(50) NOT NULL,
 "taskComplete" VARCHAR(3));
---
+++ server/public/scripts/client.js
@@ -0,0 +1,5 @@
+console.log('hello form js');
+
+$( document ).ready( function(){
+    console.log( 'JQ' );
+});
