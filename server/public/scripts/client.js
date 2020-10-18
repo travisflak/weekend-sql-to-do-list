5e9eacbef371f5a318d969e471bf5f9c44aa5b61
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-17 22:26:38 -0500

    I have a GET request with appending to the DOM and arguments, but not currently working due to syntax error

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -17,7 +17,41 @@ function setupClickListeners() {
 
 //GET
 function getToDos() {
-
+    let el = $('#viewToDos');
+    el.empty();
+    console.log('in getToDos');
+    //ajax call to server to get todos
+    $.ajax( {
+        method: 'GET',
+        url: '/router'
+    }).then( function ( response ) {
+        console.log('back from server with', response );
+        for (let i = 0; i < response.length; i++) {
+            if (response[i]).taskComplete == "y" || (response[i]).taskComplete == "Y") {
+                el.append(`
+                    <tr>
+                        <td>${respnse[i].person}</td>
+                        <td>${respnse[i].taskName}</td>
+                        <td>${respnse[i].taskNotes}</td>
+                        <td>${respnse[i].taskComplete}</td>
+                    </tr>
+                `)
+            } else {
+                el.append (`
+                    <tr data-id=${response[i]}>
+                    <td>${respnse[i].person}</td>
+                        <td>${respnse[i].taskName}</td>
+                        <td>${respnse[i].taskNotes}</td>
+                        <td>${respnse[i].taskComplete}</td>
+                        <td><button class="ready">Ready</button></td>
+                    </tr>
+                `)
+            }
+            }
+    }).catch(function(error) {
+        console.log('error', error);
+        res.sendStatus(500)
+    });
 }//end getToDos
 
 
@@ -31,7 +65,7 @@ function saveToDo( newToDo ) {
         type: 'POST',
         url: '/router',
         data: newToDo,
-    }).then (function(response) {
+    }).then ( function ( response ) {
         $('#person').val(''),
         $('#taskName').val(''),
         $('#taskNotes').val(''),
