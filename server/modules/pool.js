b0a679af831b6eaa85e90422a47fad9691c1fcb1
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-20 07:31:47 -0500

    deploying on heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 
 const Pool = pg.Pool;
 const pool = new Pool({
-    database: 'weekend-to-do-app', //name of the database 'weekend-to-do-app' goes here
+    database: process.env.DATABASE_NAME || 'weekend-to-do-app', //name of the database 'weekend-to-do-app' goes here
     host: 'localhost',
     port: 5432,
     max: 10, 
